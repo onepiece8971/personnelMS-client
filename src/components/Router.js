2d2bb4a2f9c50225a97eafbc414b5643690012b8
chenglinz <zhangchenglin@pxsj.com>
2018-09-14 18:15:08 +0800

    fix router

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from './Login'
 
 const AppRouter = () => (
-  <Router history={history}>
+  <Router>
     <Route path="/" component={Login}/>
   </Router>
 )
